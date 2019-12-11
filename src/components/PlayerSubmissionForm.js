03cdadda8356c726882b6d00464d4a25a3a6b700
Dianna Tingg <dtingg@gmail.com>
2019-12-10 16:10:34 -0800

    added fields for player submission form

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -16,14 +16,41 @@ class PlayerSubmissionForm extends Component {
         <form className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-
             {
               // Put your form inputs here... We've put in one below as an example
             }
+            <span>The</span>
+
             <input
-              placeholder="hm..."
+              placeholder="adjective"
+              type="text" />
+            
+            <input
+              placeholder="noun"
               type="text" />
 
+            <input
+              placeholder="adverb"
+              type="text" />
+            
+            <input
+              placeholder="verb"
+              type="text" />
+
+            <span>the</span>
+
+            <input
+              placeholder="adjective"
+              type="text" />
+
+            <input
+              placeholder="noun"
+              type="text" />
+
+            <span>.</span>
+            {/* <input
+              placeholder="hm..."
+              type="text" /> */}
           </div>
 
           <div className="PlayerSubmissionForm__submit">
